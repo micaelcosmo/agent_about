980bef93f5b6b87d5ee1a89b14f298ffc1fdf86c
MicaelCosmo <micael.trabalho@hotmail.com>
2026-03-07 17:25:48 -0300

    refactor: Changed OPENAI for HuggingFace
    O único motivo é o valor cobrado para essa operação embedding.

--- schema.py
+++ schema.py
@@ -1,21 +1,19 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 BASE_DIR = os.getenv('BASE_DIR')
 DB_DIR = os.getenv('DB_DIR')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
 
 def create_db():
     documents = load_documents()
     chunks = split_chunks(documents=documents)
     vectorize_chunks(chunks)
-    return
+    print("Banco de dados vetorial criado com sucesso!")
 
 def load_documents():
     _loader = PyPDFDirectoryLoader(BASE_DIR, glob="*.pdf")
@@ -24,8 +22,8 @@ def load_documents():
 
 def split_chunks(documents):
     document_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
+        chunk_size=4000,
+        chunk_overlap=500,
         length_function=len,
         add_start_index=True
     )
@@ -33,7 +31,14 @@ def split_chunks(documents):
     return chunks
 
 def vectorize_chunks(chunks):
-    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory=DB_DIR)
+    # Utilizando um modelo open-source e gratuito que roda localmente
+    embeddings_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    
+    db = Chroma.from_documents(
+        documents=chunks, 
+        embedding=embeddings_model, 
+        persist_directory=DB_DIR
+    )
 
 if __name__ == "__main__":
     create_db()
